0573872732a749329bacdfca6cb6002d0381a726
unknown <consultorio@HCB.ORG.BR>
2021-03-14 17:09:18 -0300

    subindo aplicação para testes no postman

--- payrollTest/src/main/java/com/payroll/controller/EmployeeController.java
+++ payrollTest/src/main/java/com/payroll/controller/EmployeeController.java
@@ -2,12 +2,14 @@ package com.payroll.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payroll.entity.Employee;
@@ -27,18 +29,20 @@ public class EmployeeController {
 	}
 
 	@GetMapping("/employees")
+	@ResponseStatus(HttpStatus.CREATED)
 	List<Employee> all() {
 		return repository.findAll();
 	}
-	
-	 @GetMapping("/employees/{id}")
-	  Employee one(@PathVariable Long id) {
-	    
-	    return repository.findById(id)
-	      .orElseThrow(() -> new EmployeeNotFoundException(id));
-	  }
 
+	@GetMapping("/employees/{id}")
+	Employee one(@PathVariable Long id) {
+
+		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+	}
+
+	
 	@PostMapping("/employees")
+	@ResponseStatus(HttpStatus.CREATED)
 	Employee newEmployee(@RequestBody Employee newEmployee) {
 		return repository.save(newEmployee);
 	}
@@ -56,7 +60,7 @@ public class EmployeeController {
 			return repository.save(newEmployee);
 		});
 	}
-	
+
 	@DeleteMapping("/employees/{id}")
 	void deleteEmployee(@PathVariable Long id) {
 		repository.deleteById(id);
--- payrollTest/src/main/java/com/payroll/entity/Employee.java
+++ payrollTest/src/main/java/com/payroll/entity/Employee.java
@@ -2,6 +2,7 @@ package com.payroll.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -19,7 +20,7 @@ public class Employee {
 	}
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
